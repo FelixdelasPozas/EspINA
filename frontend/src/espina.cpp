78830d80195db985022ccc2f56ede8e38326d26e
Jorge Peña Pastor <jorge.pena.pastorn@gmail.com>
2010-09-16 13:00:10 +0200

    Espina project structure
    Two independent subprojects have been created:
    - backend: server side program to manage the computational load impossed by the volume of the input data.
    - frontend: light client to interact with the backend.

---
+++ frontend/src/espina.cpp
@@ -0,0 +1,5 @@
+#include "espina.h"
+
+Espina::Espina()
+{
+}
---
+++ frontend/src/espina.h
@@ -0,0 +1,14 @@
+//@author: jmorales, jgpeña
+
+#ifndef ESPINA_H
+#define ESPINA_H
+
+#include <QMainWindow>
+
+class Espina : public QMainWindow
+{
+public:
+    Espina();
+};
+
+#endif // ESPINA_H
