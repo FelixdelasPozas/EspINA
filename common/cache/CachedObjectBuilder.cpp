FelixdelasPozas/EspINA

commit 2561ef0958040a66823003c2173b673ad4f631ec

Apply CachedObjectBuilder patch to use named inputs. pqFilters without inputs don't need to add an empty pqFilter's INPUT argument

common/cache/CachedObjectBuilder.cpp
@@ -125,63 +125,76 @@ Cache::Index CachedObjectBuilder::generateId(const QString group, const QString 
 pqPipelineSource *CachedObjectBuilder::createSMFilter(const QString group, const QString name, const pqFilter::Arguments args)
 {
   pqApplicationCore* core = pqApplicationCore::instance();
-  pqObjectBuilder* ob = core->getObjectBuilder();
-  
-//   CACHE_DEBUG("CachedObjectBuilder: Create Filter " << name);
-  pqPipelineSource *filter; //= builder->createFilter(group, name,NULL);
+  pqObjectBuilder* ob     = core->getObjectBuilder();
+  pqServer *activeServer  = core->getActiveServer();
+
+  //   CACHE_DEBUG("CachedObjectBuilder: Create Filter " << name);
+  pqPipelineSource *filter = NULL;
   vtkSMProperty *p;
+
+  QMap<QString, QList<pqOutputPort*> > namedInputs;
+  foreach (pqFilter::Argument arg, args)
+  {
+    if (arg.type == pqFilter::Argument::INPUT)
+    {
+	QStringList input = arg.value.split(":");
+	Q_ASSERT(input.size()==2);
+	pqFilter *inputCreator = m_cache->getEntry(input[0]);
+	Q_ASSERT(inputCreator);
+	pqOutputPort *port = inputCreator->pipelineSource()->getOutputPort(input[1].toInt());
+	Q_ASSERT(port);
+	// Recover already connected output ports for this input
+	if (namedInputs.contains(arg.name))
+	  namedInputs[arg.name] << port;
+	else
+	  namedInputs[arg.name] = (QList<pqOutputPort*>() << port);
+    }
+  }
+
+  // Filter is a source
+  if (namedInputs.isEmpty())
+    filter = ob->createSource(group, name, activeServer);
+  else
+    filter = ob->createFilter(group, name, namedInputs, activeServer);
+
+  Q_ASSERT(filter != NULL);
+
   foreach (pqFilter::Argument arg, args)
   {
     switch (arg.type)
     {
       case pqFilter::Argument::INPUT:
-      {
-	// Filter is a source
-	if (arg.value == "")
-	{
-	  filter = ob->createSource(group, name, pqApplicationCore::instance()->getActiveServer());
-	}
-	else
-	{
-	  QStringList input = arg.value.split(":");
-	  Q_ASSERT(input.size()==2);
-	  pqFilter *inputCreator = m_cache->getEntry(input[0]);
-	  Q_ASSERT(inputCreator);
-	  filter = ob->createFilter(group, name, inputCreator->pipelineSource(), input[1].toInt());
-	}
-      }
-      break;
+	break;
       case pqFilter::Argument::INTVECT:
-	{
-	  Q_ASSERT(filter != NULL);
-	  p = filter->getProxy()->GetProperty( arg.name.toStdString().c_str() );
-	  vtkSMIntVectorProperty * prop = vtkSMIntVectorProperty::SafeDownCast(p);
-	  QStringList values = arg.value.split(",");
-// 	  CACHE_DEBUG("CachedObjectBuilder:" << arg.name << "Values" <<  values);
-	  for (int i = 0; i < values.size(); i++)
-	    prop->SetElement(i, values[i].toInt());
-	}
+      {
+	p = filter->getProxy()->GetProperty( arg.name.toStdString().c_str() );
+	vtkSMIntVectorProperty * prop = vtkSMIntVectorProperty::SafeDownCast(p);
+	QStringList values = arg.value.split(",");
+	// 	  CACHE_DEBUG("CachedObjectBuilder:" << arg.name << "Values" <<  values);
+	for (int i = 0; i < values.size(); i++)
+	  prop->SetElement(i, values[i].toInt());
+
 	break;
+      }
       case pqFilter::Argument::DOUBLEVECT:
-	{
-	  Q_ASSERT(filter != NULL);
-	  p = filter->getProxy()->GetProperty( arg.name.toStdString().c_str() );
-	  vtkSMDoubleVectorProperty * prop = vtkSMDoubleVectorProperty::SafeDownCast(p);
-	  QStringList values = arg.value.split(",");
-// 	  qDebug() << "CachedObjectBuilder:" << arg.name << "Values" <<  values;
-	  for (int i = 0; i < values.size(); i++)
-	    prop->SetElement(i, values[i].toDouble());
-	}
+      {
+	p = filter->getProxy()->GetProperty( arg.name.toStdString().c_str() );
+	vtkSMDoubleVectorProperty * prop = vtkSMDoubleVectorProperty::SafeDownCast(p);
+	QStringList values = arg.value.split(",");
+	// 	  qDebug() << "CachedObjectBuilder:" << arg.name << "Values" <<  values;
+	for (int i = 0; i < values.size(); i++)
+	  prop->SetElement(i, values[i].toDouble());
+
 	break;
+      }
       default:
-// 	qDebug() << "Unkown parameter type";
+	// 	qDebug() << "Unkown parameter type";
 	Q_ASSERT(false);
     };
   }
-  Q_ASSERT(filter != NULL);
-  //TODO: Review if needed here
-  filter->getProxy()->UpdateVTKObjects();
- return filter;
+
+//   filter->getProxy()->UpdateVTKObjects();
+  return filter;
 }
 
 pqFilter* CachedObjectBuilder::registerFilter(const QString id, pqPipelineSource* source)

plugins/SeedGrowSegmentation/SeedGrowSegmentation.cpp
@@ -237,36 +237,7 @@ void SeedGrowSegmentation::startSegmentation(SelectionHandler::MultiSelection ms
 
     QSharedPointer<QUndoStack> undo(EspinaCore::instance()->undoStack());
     undo->push(new UndoCommand(channel, filter, tax));
-
-  // args.insert("VOI",SelectionManager::instance()->voi()->save());
-  //createFilter(m_pluginName + "::" + "SeedGrowSegmentationFilter",args);createFilter(m_pluginName + "::" + "SeedGrowSegmentationFilter",args);
-
-//   IVOI *voi = SelectionManager::instance()->voi();
-//   if (!voi && m_useDefaultVOI->isChecked())
-//   {
-//     voi = m_defaultVOI;
-//     Sample *input = EspinaModel::instance()->activeSample();
-//     voi->setSource(input);
-//     double spacing[3];
-//     input->spacing(spacing);
-//     double defVOI[6] = {(seed.x - m_preferences->xSize())*spacing[0],
-// 			(seed.x + m_preferences->xSize())*spacing[0],
-// 			(seed.y - m_preferences->ySize())*spacing[1],
-// 			(seed.y + m_preferences->ySize())*spacing[1],
-// 			(seed.z - m_preferences->zSize())*spacing[2],
-// 			(seed.z + m_preferences->zSize())*spacing[2]};
-//     vtkSMPropertyHelper(voi->getProxy(),"Bounds").Set(defVOI,6);
-//     voi->getProxy()->UpdateVTKObjects();
-// //     double checkBounds[6];
-//     vtkSMPropertyHelper(voi->getProxy(),"Bounds").Get(defVOI,6);
-//   }
-
-//     m_lastFilter = new SeedGrowSegmentationFilter(args);
-//   if (!sgs_sgsf)
-//     qWarning() << "SeedGrowSegmentation: Failed to create new segmentation";
   }
-
-//   QApplication::restoreOverrideCursor();
 }
 
 //-----------------------------------------------------------------------------

plugins/SeedGrowSegmentation/vtkConnectedThresholdImageFilter.h
@@ -9,7 +9,7 @@ class VTK_IMAGING_EXPORT vtkConnectedThresholdImageFilter :
 public:
   static vtkConnectedThresholdImageFilter *New();
   vtkTypeMacro(vtkConnectedThresholdImageFilter,vtkImageAlgorithm);
-    
+
   //! Gray level segmentation threshlod
   vtkSetMacro(Threshold,int);
   vtkGetMacro(Threshold,int);
@@ -20,13 +20,13 @@ public:
   void SetCheckPixel(int x, int y, int z, int value);
   void SetCheckPixel(int arg[4]);
   vtkGetVector4Macro(CheckPixel,int);
-  
+
   vtkGetMacro(PixelValue,int);
 
   vtkGetVector6Macro(SegExtent,int);
-  
+
   void PrintSelf(ostream& os, vtkIndent indent);
-    
+
 protected:
 
   vtkConnectedThresholdImageFilter();

commit cc4cc635ba10a8181898832d7f776af7702bd612

Bug Fix: Regression introduced in commit 2561ef095

common/cache/CachedObjectBuilder.cpp
@@ -193,7 +193,7 @@ pqPipelineSource *CachedObjectBuilder::createSMFilter(const QString group, const
     };
   }
 
-//   filter->getProxy()->UpdateVTKObjects();
+  filter->getProxy()->UpdateVTKObjects();
   return filter;
 }
 

frontend/EspinaWindow.cpp
@@ -210,9 +210,9 @@ EspinaWindow::EspinaWindow()
 //   m_mainToolBar->setMovable(false);
   addToolBar(m_mainToolBar);
 
-  QToolBar *lod = new LODToolBar();
-//   lod->setMovable(false);
-  addToolBar(lod);
+//   QToolBar *lod = new LODToolBar();
+// //   lod->setMovable(false);
+//   addToolBar(lod);
 
   ChannelExplorer *channelExplorer = new ChannelExplorer(m_model, this);
   addDockWidget(Qt::LeftDockWidgetArea, channelExplorer);