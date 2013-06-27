8a83fb023060fe8755d8fbe567e821854675f319
Juan Morales <juan.morales@upm.es>
2013-06-27 15:00:40 +0200

    espina icon in svg

--- App/main.cpp
+++ App/main.cpp
@@ -32,7 +32,7 @@
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
-  app.setWindowIcon(QIcon(":/espina/espina.png"));
+  app.setWindowIcon(QIcon(":/espina/espina.svg"));
 
   QTranslator translator;
   translator.load("espina_es");
