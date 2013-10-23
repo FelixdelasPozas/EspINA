98f87f9cfc07300552e250248a98ee4600b6ec1b
Félix de las Pozas Álvarez <fpozas@alumnos.upm.es>
2013-10-23 22:11:30 +0200

    Binary Mask and iterators update. Not tested.

--- Core/Utils/BinaryMask.cpp
+++ Core/Utils/BinaryMask.cpp
@@ -17,18 +17,163 @@
  *
  */
 
+// EspINA
 #include "BinaryMask.h"
+#include <Core/EspinaTypes.h>
+
+// C++
 #include <memory>
+#include <math.h>
+#include <string.h>
+#include <cmath>
 
-using namespace EspINA;
+// itk
+#include <itkImageRegion.h>
+#include <itkImageRegionConstIterator.h>
 
-BinaryMask::BinaryMask(const Bounds& bounds)
-: m_bounds{bounds}
+namespace EspINA
 {
-}
+
+  //-------------------------------------------------------------------------------------
+  template<typename T> BinaryMask<T>::BinaryMask(const Bounds& bounds, Spacing spacing)
+  : m_bounds{bounds}
+  , m_backgroundValue(0)
+  , m_foregroundValue(255)
+  , m_integerSize(sizeof(int)*8)
+  , m_spacing(spacing)
+  {
+    if (!bounds.areValid())
+      throw;
+
+    m_size[0] = (std::floor(bounds[1]/m_spacing.x) - std::ceil(bounds[0]/m_spacing.x)) + 1;
+    m_size[1] = (std::floor(bounds[3]/m_spacing.y) - std::ceil(bounds[2]/m_spacing.y)) + 1;
+    m_size[2] = (std::floor(bounds[5]/m_spacing.z) - std::ceil(bounds[4]/m_spacing.z)) + 1;
+
+    m_origin.x = static_cast<int>(std::floor(m_bounds[0]/m_spacing.x));
+    m_origin.y = static_cast<int>(std::floor(m_bounds[2]/m_spacing.y));
+    m_origin.z = static_cast<int>(std::floor(m_bounds[4]/m_spacing.z));
+
+    long int bufferSize = m_size[0] * m_size[1] * m_size[2];
+    m_image = new int[bufferSize];
+    memset(m_image, 0, bufferSize*sizeof(int));
+  }
+
+  //-------------------------------------------------------------------------------------
+  template<typename T> void BinaryMask<T>::setPixel(IndexType& index)
+  {
+    Nm point[3] = { index.x*m_spacing.x, index.y*m_spacing.y, index.z*m_spacing.z };
+    if (point[0] < m_bounds[0] || point[0] > m_bounds[1] ||
+        point[1] < m_bounds[2] || point[1] > m_bounds[3] ||
+        point[2] < m_bounds[4] || point[2] > m_bounds[5])
+    {
+      throw;
+    }
+
+    // we must adjust the index
+    index.x = index.x - m_origin.x;
+    index.y = index.y - m_origin.y;
+    index.z = index.z - m_origin.z;
+    unsigned long valuePosition = index.x + (index.y * m_size[1]) + (index.z * m_size[2]*m_size[1]);
+    unsigned long imageOffset = valuePosition / m_integerSize;
+    unsigned long valueOffset = valuePosition % m_integerSize;
+
+    m_image[imageOffset] = m_image[imageOffset] & (1 << valueOffset);
+  }
+
+  //-------------------------------------------------------------------------------------
+  template<typename T> void BinaryMask<T>::unsetPixel(IndexType& index)
+  {
+    Nm point[3] = { index.x*m_spacing.x, index.y*m_spacing.y, index.z*m_spacing.z };
+    if (point[0] < m_bounds[0] || point[0] > m_bounds[1] ||
+        point[1] < m_bounds[2] || point[1] > m_bounds[3] ||
+        point[2] < m_bounds[4] || point[2] > m_bounds[5])
+    {
+      throw;
+    }
+
+    // we must adjust the index
+    index.x = index.x - m_origin.x;
+    index.y = index.y - m_origin.y;
+    index.z = index.z - m_origin.z;
+    unsigned long valuePosition = index.x + (index.y * m_size[1]) + (index.z * m_size[2]*m_size[1]);
+    unsigned long imageOffset = valuePosition / m_integerSize;
+    unsigned long valueOffset = valuePosition % m_integerSize;
+
+    m_image[imageOffset] = m_image[imageOffset] &= ~(1 << valueOffset);;
+  }
+
+  //-------------------------------------------------------------------------------------
+  template<typename T> typename BinaryMask<T>::PixelType BinaryMask<T>::pixel(IndexType& index) const
+  {
+    Nm point[3] = { index.x*m_spacing.x, index.y*m_spacing.y, index.z*m_spacing.z };
+    if (point[0] < m_bounds[0] || point[0] > m_bounds[1] ||
+        point[1] < m_bounds[2] || point[1] > m_bounds[3] ||
+        point[2] < m_bounds[4] || point[2] > m_bounds[5])
+    {
+      throw;
+    }
+
+    // we must adjust the index
+    index.x = index.x - m_origin.x;
+    index.y = index.y - m_origin.y;
+    index.z = index.z - m_origin.z;
+    unsigned long valuePosition = index.x + (index.y * m_size[1]) + (index.z * m_size[2]*m_size[1]);
+    unsigned long imageOffset = valuePosition / m_integerSize;
+    unsigned long valueOffset = valuePosition % m_integerSize;
+
+    bool set = ((m_image[imageOffset] & (1 << valueOffset)) == (1 << valueOffset));
+    if (set)
+      return m_foregroundValue;
+
+    return m_backgroundValue;
+  }
+
+  //-------------------------------------------------------------------------------------
+  template<typename T> typename BinaryMask<T>::itkImageType::Pointer BinaryMask<T>::itkImage() const
+  {
+    using ImagePointer = typename itkImageType::Pointer;
+    using ImageRegion = typename itkImageType::RegionType;
+    using ImageIndex = typename itkImageType::RegionType::IndexType;
+    using ImageSize = typename itkImageType::RegionType::SizeType;
+    using ImageSpacing = typename itkImageType::SpacingType;
+    using ImageIterator = typename itk::ImageRegionConstIterator<itkImageType>;
+    using MaskIterator = typename BinaryMask<T>::Iterator;
+
+    ImageIndex index;
+    index[0] = m_origin[0];
+    index[1] = m_origin[1];
+    index[2] = m_origin[2];
+    ImageSize size;
+    size[0] = m_size[0];
+    size[1] = m_size[1];
+    size[2] = m_size[2];
+    ImageRegion region;
+    region.SetIndex(index);
+    region.SetSize(size);
+    ImageSpacing spacing;
+    spacing[0] = m_spacing.x;
+    spacing[1] = m_spacing.y;
+    spacing[2] = m_spacing.z;
+
+    ImagePointer image = itkImageType::New();
+    image->SetOrigin(index);
+    image->SetRegions(region);
+    image->SetSpacing(spacing);
+    image->Allocate();
+    image->FillBuffer(m_backgroundValue);
+
+    ImageIterator iit(image, region);
+    MaskIterator mit(m_image, m_bounds);
+
+    // TODO: use itk & mask raw buffers to make a fast copy
+    for (iit.GoToBegin(); iit != iit.End(); ++iit, ++mit)
+      *iit = mit.get();
+
+    return image;
+  }
+
+  //-------------------------------------------------------------------------------------
+  // TODO: iterators implementation
+};
 
 
-Bounds BinaryMask::bounds() const
-{
-  return m_bounds;
-}
--- Core/Utils/BinaryMask.h
+++ Core/Utils/BinaryMask.h
@@ -17,23 +17,117 @@
  *
  */
 
-#ifndef ESPINA_BINARYMASK_H
-#define ESPINA_BINARYMASK_H
+#ifndef ESPINA_BINARY_MASK_H
+#define ESPINA_BINARY_MASK_H
 
 #include "Core/Utils/Bounds.h"
 
-namespace EspINA {
+#include "itkImage.h"
+#include <itkSmartPointer.h>
 
-  class BinaryMask
+namespace EspINA
+{
+  //- Binary Mask class -----------------------------------------------------------------
+  template<typename T> class BinaryMask
   {
-  public:
-    explicit BinaryMask(const Bounds& bounds);
+    using PixelType    = T;
+    using IndexType    = struct index { int x; int y; int z; index(): x(0), y(0), z(0) {} };
+    using Spacing      = struct spacing { int x; int y; int z; spacing(): x(0), y(0), z(0) {} };
+    using itkImageType = itk::Image<T,3>;
 
-    Bounds bounds() const;
+    class Iterator;
+    class ConstIterator;
+    class RegionIterator;
+    class RegionConstIterator;
 
-  private:
-    Bounds m_bounds;
+    public:
+      explicit BinaryMask(const Bounds& bounds, const Spacing spacing);
+      virtual ~BinaryMask();
+
+      Bounds bounds() const                      { return m_bounds; }
+
+      PixelType backgroundValue() const          { return m_backgroundValue; }
+      void setBackgroundValue(T backgroundValue) { m_backgroundValue = backgroundValue; }
+
+      PixelType foregroundValue() const          { return m_foregroundValue; }
+      void setForegroundValue(T foregroundValue) { m_foregroundValue = foregroundValue; }
+
+      inline void setPixel(IndexType& index);
+      inline void unsetPixel(IndexType& index);
+      inline PixelType pixel(IndexType& index) const;
+
+      typename itkImageType::Pointer itkImage() const;
+
+    private:
+      Bounds        m_bounds;
+      PixelType     m_backgroundValue;
+      PixelType     m_foregroundValue;
+      int          *m_image;
+      int           m_integerSize;
+      Spacing       m_spacing;
+      unsigned long m_size[3];
+      IndexType     m_origin;
+
+      friend class Iterator;
+      friend class ConstIterator;
+      friend class RegionIterator;
+      friend class RegionConstIterator;
   };
+
+  //- Binary Mask class iterator --------------------------------------------------------
+  template<typename T> class BinaryMask<T>::Iterator
+  {
+    public:
+      Iterator(BinaryMask<T> image, Bounds bounds);
+      ~Iterator();
+
+      T get() const;
+      void set();
+
+      Iterator& operator++();
+      Iterator& operator--();
+  };
+
+  //- Binary Mask class const iterator --------------------------------------------------
+  template<typename T> class BinaryMask<T>::ConstIterator
+  {
+    public:
+      ConstIterator(BinaryMask<T> image, Bounds bounds);
+      ~ConstIterator();
+
+      const T get() const;
+
+      ConstIterator& operator++();
+      ConstIterator& operator--();
+  };
+
+  //- Binary Mask region iterator--------------------------------------------------------
+  template<typename T> class BinaryMask<T>::RegionIterator
+  {
+    public:
+      RegionIterator(BinaryMask<T> image, Bounds bounds);
+      ~RegionIterator();
+
+      T get() const;
+      void set();
+
+      RegionIterator& operator++();
+      RegionIterator& operator--();
+  };
+
+  //- Binary Mask region const interator ------------------------------------------------
+  template<typename T> class BinaryMask<T>::RegionConstIterator
+  {
+    public:
+      RegionConstIterator(BinaryMask<T> image, Bounds bounds);
+      ~RegionConstIterator();
+
+      const T get() const;
+
+      RegionConstIterator& operator++();
+      RegionConstIterator& operator--();
+  };
+
 }
 
-#endif // ESPINA_BINARYMASK_H
+#endif // ESPINA_BINARY_MASK_H
